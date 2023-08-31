d323721dc4e840942704de7003a6aab768d7e272
jaehanbyun <awbrg789@naver.com>
2023-09-01 02:06:40 +0900

    Feat: getVolumes API 구현

--- src/app/app.go
+++ src/app/app.go
@@ -1,9 +1,14 @@
 package app
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/jaehanbyun/VM-Disaster-Recovery/data"
 	"github.com/jaehanbyun/VM-Disaster-Recovery/model"
 	"github.com/unrolled/render"
 	"github.com/urfave/negroni"
@@ -38,6 +43,93 @@ func enableCORS(h http.Handler) http.Handler {
 	})
 }
 
+func (a *Apphandler) getVolumes(w http.ResponseWriter, r *http.Request) {
+	token := getToken()
+	req, err := http.NewRequest("GET", "http://"+baseOpenstackUrl+"/v3"+projectId+"/volumes/detail", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	req.Header.Set("X-Auth-Token", token)
+	req.Header.Set("content-type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+	var volumeList data.VolumeListResponse
+	json.Unmarshal(body, &volumeList)
+
+	summaryMap := make(map[string]data.Summary)
+
+	for _, v := range volumeList.Volumes {
+		vType := v.Metadata.Type
+		if _, ok := summaryMap[vType]; !ok {
+			summaryMap[vType] = data.Summary{}
+		}
+		summaryMap[vType] = data.Summary{ID: append(summaryMap[vType].ID, v.ID)}
+	}
+
+	finalOutput := map[string]map[string]data.Summary{
+		"volumes": summaryMap,
+	}
+
+	jsonData, err := json.Marshal(finalOutput)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rd.JSON(w, http.StatusOK, jsonData)
+}
+
+func getToken() string {
+	payload := data.Payload{
+		Auth: data.Auth{
+			Identity: data.Identity{
+				Methods: []string{"password"},
+				Password: data.Password{
+					User: data.User{
+						Name:     "admin",
+						Domain:   "Default",
+						Password: "0000",
+					},
+				},
+			},
+			Scope: data.Scope{
+				System: data.System{
+					All: true,
+				},
+			},
+		},
+	}
+
+	body, err := json.Marshal(payload)
+
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	}
+
+	buff := bytes.NewBuffer(body)
+	resp, err := http.Post("http://"+baseOpenstackUrl+"identity/v3/auth/tokens", "application/json", buff)
+
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	} else {
+		token := resp.Header.Get("X-Subject-Token")
+		return token
+	}
+
+	return ""
+}
+
+func findSimilarVM(id string) *data.VMInstance {
+	return &data.VMInstance{}
+}
+
 func MakeHandler() *Apphandler {
 	rd = render.New()
 	r := mux.NewRouter()
@@ -50,6 +142,8 @@ func MakeHandler() *Apphandler {
 		Handler: neg,
 		db:      model.NewDBHandler(),
 	}
+
+	r.HandleFunc("/volumes", a.getVolumes).Methods("GET")
 
 	return a
 }
--- src/data/data.go
+++ src/data/data.go
@@ -18,3 +18,53 @@ type VMInstance struct {
 	OS       string   `json:"os"`
 	Software Software `json:"software"`
 }
+
+type Payload struct {
+	Auth Auth `json:"auth"`
+}
+
+type Auth struct {
+	Identity Identity `json:"identity"`
+	Scope    Scope    `json:"scope"`
+}
+
+type Scope struct {
+	System System `json:"system"`
+}
+
+type System struct {
+	All bool `json:"all"`
+}
+
+type Identity struct {
+	Methods  []string `json:"methods"`
+	Password Password `json:"password"`
+}
+
+type Password struct {
+	User User `json:"user"`
+}
+
+type User struct {
+	Name     string `json:"name"`
+	Domain   string `json:"domain"`
+	Password string `json:"password"`
+}
+
+type VolumeDetail struct {
+	ID       string   `json:"id"`
+	Metadata Metadata `json:"metadata"`
+}
+
+type Metadata struct {
+	Type    string `json:"type"`
+	Content string `json:"content"`
+}
+
+type VolumeListResponse struct {
+	Volumes []VolumeDetail `json:"volumes"`
+}
+
+type Summary struct {
+	ID []string `json:"id"`
+}
