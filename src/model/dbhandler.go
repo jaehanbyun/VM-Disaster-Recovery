64a729ac9883d6ea573392282e428cf53b0f0779
jaehanbyun <awbrg789@naver.com>
2023-09-01 02:10:58 +0900

    Feat: Threshold 관련 DB Interface Method 구현

--- src/model/dbhandler.go
+++ src/model/dbhandler.go
@@ -2,7 +2,6 @@ package model
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 
 	"github.com/jaehanbyun/VM-Disaster-Recovery/data"
@@ -30,6 +29,21 @@ func (p *postgresHandler) GetWeight() (data.Weight, error) {
 func (p *postgresHandler) SetWeight(weight data.Weight) error {
 	_, err := p.db.Exec("INSERT INTO weight (language, database, webserver, threshold) VALUES ($1 $2 $3 $4) ON CONFLICT DO UPDATE",
 		weight.Language, weight.Database, weight.Webserver, weight.Threshold)
+	return err
+}
+
+func (p *postgresHandler) GetThreshold() (float32, error) {
+	row := p.db.QueryRow("SELECT threshold FROM weight LIMIT 1")
+	var threshold float32
+	err := row.Scan(&threshold)
+	if err != nil {
+		return threshold, err
+	}
+	return threshold, nil
+}
+
+func (p *postgresHandler) SetThreshold(t float32) error {
+	_, err := p.db.Exec("INSERT INTO weight (threshold) VALUES ($1) ON CONFLICT DO UPDATE", t)
 	return err
 }
 
--- src/model/model.go
+++ src/model/model.go
@@ -5,6 +5,8 @@ import "github.com/jaehanbyun/VM-Disaster-Recovery/data"
 type DBHandler interface {
 	GetWeight() (data.Weight, error)
 	SetWeight(data.Weight) error
+	GetThreshold() (float32, error)
+	SetThreshold(float32) error
 	Close()
 }
 
