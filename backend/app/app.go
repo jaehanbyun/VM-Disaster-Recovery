6573206ce2dedc0ab97e3d7ca365f6f8e42ba5aa
jaehanbyun <awbrg789@naver.com>
2023-09-12 20:57:16 +0900

    Feat: recoverInstance API 추가

--- backend/app/app.go
+++ backend/app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -130,6 +131,160 @@ func (a *AppHandler) createInstance(w http.ResponseWriter, r *http.Request) {
 	rd.Text(w, http.StatusOK, "OK")
 }
 
+func (a *AppHandler) recoverInstance(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	targetVM, err := a.db.GetVMInfo(id)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error fetching VM with ID: %s. Error: %v", id, err), http.StatusInternalServerError)
+		return
+	}
+
+	allVMs, err := a.db.GetVMsInfo()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error fetching all VMs: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	weights, err := a.db.GetWeight()
+	maxSimilarity := 0.0
+	var mostSimilarVM *data.VMInstance
+
+	for _, vm := range allVMs {
+		if vm.ID == targetVM.ID {
+			continue
+		}
+		similarity := calculateSimilarity(weights, *targetVM, *vm)
+		if similarity > maxSimilarity {
+			maxSimilarity = similarity
+			mostSimilarVM = vm
+		}
+	}
+
+	if maxSimilarity >= float64(weights.Threshold) {
+		var nonOverlappingVolumes []string
+		for _, volume := range targetVM.Software.Languages {
+			if !contains(mostSimilarVM.Software.Languages, volume) {
+				nonOverlappingVolumes = append(nonOverlappingVolumes, volume.ID)
+			}
+		}
+		for _, volume := range targetVM.Software.Databases {
+			if !contains(mostSimilarVM.Software.Databases, volume) {
+				nonOverlappingVolumes = append(nonOverlappingVolumes, volume.ID)
+			}
+		}
+		for _, volume := range targetVM.Software.Webservers {
+			if !contains(mostSimilarVM.Software.Webservers, volume) {
+				nonOverlappingVolumes = append(nonOverlappingVolumes, volume.ID)
+			}
+		}
+
+		token := common.GetToken()
+
+		for _, volumeID := range nonOverlappingVolumes {
+			body, err := json.Marshal(makeVolumeAttachmentReq(volumeID))
+			if err != nil {
+				http.Error(w, fmt.Sprintf("failed to marshal volume attachment data: %v", err), http.StatusInternalServerError)
+				return
+			}
+			req, err := http.NewRequest("POST", common.BaseOpenstackUrl+"/compute/v2.1/servers/"+mostSimilarVM.ID+"/os-volume_attachments", bytes.NewBuffer(body))
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Error creating new request: %s", err), http.StatusInternalServerError)
+				return
+			}
+			req.Header.Set("X-Auth-Token", token)
+			req.Header.Set("content-type", "application/json")
+			// Openstack Nova Compute API Version 2.60 Include
+			req.Header.Set("Openstack-API-Version", "compute 2.60")
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				http.Error(w, fmt.Sprintf("error sending volume attachment request: %s", err), http.StatusInternalServerError)
+				return
+			}
+			resp.Body.Close()
+			if resp.StatusCode != http.StatusOK {
+				http.Error(w, "failed to attach volume", http.StatusInternalServerError)
+				return
+			}
+		}
+	} else {
+		newVMName := targetVM.Name + "-new"
+		var volumes []string
+
+		for _, langVolume := range targetVM.Software.Languages {
+			volumes = append(volumes, langVolume.ID)
+		}
+		for _, dbVolume := range targetVM.Software.Databases {
+			volumes = append(volumes, dbVolume.ID)
+		}
+		for _, wsVolume := range targetVM.Software.Webservers {
+			volumes = append(volumes, wsVolume.ID)
+		}
+
+		err := recreateTerraformScript(newVMName, targetVM.OS, targetVM.FlavorID, volumes)
+		if err != nil {
+			rd.Text(w, http.StatusInternalServerError, fmt.Sprintf("Failed to recreate VM: %s", err))
+			return
+		}
+	}
+
+	rd.Text(w, http.StatusOK, fmt.Sprintf("Created a new VM with base of VM with ID %s", targetVM.ID))
+}
+
+func makeVolumeAttachmentReq(volumeId string) data.VolumeAttachmentsRequest {
+	volumeAttachmentReq := data.VolumeAttachmentsRequest{
+		VolumeAttachment: data.VolumeAttachment{
+			VolumeID: volumeId,
+		},
+	}
+
+	return volumeAttachmentReq
+}
+
+func contains(volumes []data.Volume, vol data.Volume) bool {
+	for _, v := range volumes {
+		if v.ID == vol.ID && v.Content == vol.Content {
+			return true
+		}
+	}
+	return false
+}
+
+func calculateSimilarity(weight data.Weight, source, target data.VMInstance) float64 {
+	if source.OS != target.OS {
+		return 0.0
+	}
+
+	totalWeight := 0.0
+	similarWeight := 0.0
+
+	for _, lang := range source.Software.Languages {
+		totalWeight += float64(weight.Language)
+		if contains(target.Software.Languages, lang) {
+			similarWeight += float64(weight.Language)
+		}
+	}
+
+	for _, db := range source.Software.Databases {
+		totalWeight += float64(weight.Database)
+		if contains(target.Software.Databases, db) {
+			similarWeight += float64(weight.Database)
+		}
+	}
+
+	for _, ws := range source.Software.Webservers {
+		totalWeight += float64(weight.Webserver)
+		if contains(target.Software.Webservers, ws) {
+			similarWeight += float64(weight.Webserver)
+		}
+	}
+
+	return similarWeight / totalWeight
+}
+
 func MakeHandler() *AppHandler {
 	rd = render.New()
 	r := mux.NewRouter()
@@ -147,6 +302,13 @@ func MakeHandler() *AppHandler {
 	r.HandleFunc("/instance", a.getInstances).Methods("GET")
 	r.HandleFunc("/instance", a.createInstance).Methods("POST")
 	r.HandleFunc("/instance/{id}", a.getInstanceByID).Methods("GET")
+	r.HandleFunc("/instance/{id}/recover", a.recoverInstance).Methods("POST")
+
+	err := a.db.Init()
+
+	if err != nil {
+		panic(err)
+	}
 
 	return a
 }
